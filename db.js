842fdf5f4a7af759e4591e084a7363107187e273
Corynne Moody <cmoody@elevenfifty.org>
2021-08-26 14:01:30 -0400

    adding ssl and environment var

--- db.js
+++ db.js
@@ -1,5 +1,15 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:${encodeURIComponent(process.env.PASS)}@localhost:5432/tasks`)
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    `postgres://postgres:${encodeURIComponent(
+      process.env.PASS
+    )}@localhost:5432/tasks`,
+  { 
+      dialect: "postgres",
+      ssl: process.env.ENVIRONMENT === 'production'
+  },
 
-module.exports = db
+);
+
+module.exports = db;
